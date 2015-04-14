567e5e109a046a2bed7462d08beb6cf8ea49cd11
Nick Hewitt <nick@kryptnostic.com>
2015-04-13 17:56:37 -0700

    port credential factory and unit tests

--- js/test/spec.js
+++ js/test/spec.js
@@ -1,7 +1,8 @@
-define(['require', 'src/aes-crypto-service', 'src/crypto-service-loader'], function(require) {
+define(['require', 'src/aes-crypto-service', 'src/crypto-service-loader', 'src/credential-factory'], function(require) {
     var PASSWORD = 'crom';
     var AesCryptoService = require('src/aes-crypto-service'),
-        CryptoServiceLoader = require('src/crypto-service-loader');
+        CryptoServiceLoader = require('src/crypto-service-loader'),
+        CredentialFactory = require('src/credential-factory');
 
 
     describe('AES CryptoService', function() {
@@ -30,6 +31,30 @@ define(['require', 'src/aes-crypto-service', 'src/crypto-service-loader'], funct
             expect(btoa(key)).toBe("EX3hMH7vvRVCzE/HA2liSw==");
         });
 
+    });
+
+    describe('CredentialFactory', function() {
+        it("can generate a credential", function() {
+            var credentialPair = CredentialFactory.generateCredentialPair(PASSWORD);
+            expect(credentialPair).not.toBe(null);
+        });
+
+        it("derives correct credentials from valid encrypted salt", function() {
+            var expectedCredential = '62d97eeb667d58cc012dbdb40fbcdd6e74980106f5658c4ce2e566bea3f3bc63';
+            var encryptedSalt = {
+                "iv": "ewcVcNXbhKK463r41DFS2g==",
+                "salt": "X0jjTehInQbl5KPK0sj/J9qgu9M=",
+                "contents": "6veqEBl0TNxneQfnfpLbeRey5Yfe4oIKOqrepHn5vac="
+            };
+            var derivedCredential = CredentialFactory.deriveCredential("demo", encryptedSalt);
+            expect(derivedCredential).toBe(expectedCredential);
+        });
+
+        it("derives correct credentials from generated", function() {
+            var credentialPair = CredentialFactory.generateCredentialPair(PASSWORD);
+            var derivedCredential = CredentialFactory.deriveCredential(PASSWORD, credentialPair.encryptedSalt);
+            expect(derivedCredential).toBe(credentialPair.credential);
+        });
     });
 
     describe('crypto services loader test', function() {

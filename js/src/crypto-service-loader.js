0e3b93a453ae1a6975ec07ee349a9d132756da32
Nick Hewitt <nick@kryptnostic.com>
2015-04-16 14:49:11 -0700

    crypto service loader lazily instantiates rsa crypto service

--- js/src/crypto-service-loader.js
+++ js/src/crypto-service-loader.js
@@ -1,4 +1,5 @@
 define(['require', 'jquery', 'forge.min', 'src/password-crypto', 'src/rsa-crypto'], function(require) {
+    'use strict';
     var jquery = require('jquery'),
         Forge = require('forge.min'),
         PasswordCryptoService = require('src/password-crypto'),
@@ -15,94 +16,86 @@ define(['require', 'jquery', 'forge.min', 'src/password-crypto', 'src/rsa-crypto
         if (!(this instanceof CryptoServiceLoader)) {
             throw new TypeError("CryptoServiceLoader constructor cannot be called as a function.");
         }
-        loadRsaKeys().done(function(privateKey, publicKey) {
-            this.rsaCryptoService = new RsaCryptoService(privateKey, publicKey);
-        });
-        this.rsaCryptoService = getCryptoService();
+    };
+
+    // async wrapper in case keys have not yet instantiated from constructor call.
+    function getRsaCryptoService() {
+        var deferred = new jquery.Deferred();
+        if (typeof this.rsaCryptoService === 'undefined') {
+            loadRsaKeys().then(function(keypair) {
+                this.rsaCryptoService = new RsaCryptoService(keypair.privateKey, keypair.publicKey);
+                deferred.resolve(this.rsaCryptoService);
+            }.bind(this));
+        } else {
+            deferred.resolve(this.rsaCryptoService);
+        }
+        return deferred.promise();
     };
 
     function loadRsaKeys() {
-        var deferred = jquery.Deferred(),
+        var deferred = new jquery.Deferred(),
             passwordCryptoService = new PasswordCryptoService("demo");
-        jquery.when(
-            loadPrivateKey(),
-            loadPublicKey()
-        ).then(function(encryptedPrivateKey, encryptedPublicKey) {
-            var privateKeyBytes = passwordCryptoService.decrypt(encryptedPrivateKey[0]);
+        var request = jquery.ajax({
+            url: BASE_URL + DIR_URL + PRIV_URL,
+            type: 'GET',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('X-Kryptnostic-Principal', 'krypt|vader');
+                xhr.setRequestHeader('X-Kryptnostic-Credential', 'fd81f8a7af1cb138bdce93350768b2b453ccf238908091501b05fe25616168b0');
+            }
+        });
+
+        request.done(function(blockCiphertext) {
+            var privateKeyBytes = passwordCryptoService.decrypt(blockCiphertext);
             var privateKeyBuffer = Forge.util.createBuffer(privateKeyBytes, 'raw');
             var privateKeyAsn1 = Forge.asn1.fromDer(privateKeyBuffer);
             var privateKey = Forge.pki.privateKeyFromAsn1(privateKeyAsn1);
-            // decrypt the RSA keys with password service
-            // create a forge RSA key for each
-            deferred.resolve(privateKey);
+            var publicKey = Forge.pki.setRsaPublicKey(privateKey.n, privateKey.e);
+
+            deferred.resolve({
+                privateKey: privateKey,
+                publicKey: publicKey
+            });
         });
 
-    return deferred.promise;
-};
-
-function loadPrivateKey() {
-    return jquery.ajax({
-        url: BASE_URL + DIR_URL + PRIV_URL,
-        type: 'GET',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Kryptnostic-Principal', 'krypt|vader');
-            xhr.setRequestHeader('X-Kryptnostic-Credential', 'fd81f8a7af1cb138bdce93350768b2b453ccf238908091501b05fe25616168b0');
-        },
-        success: function(data) {
-            return data;
-        }
-    });
-};
-
-function loadPublicKey() {
-    // get current user from document cookies, throw error if not logged in
-    var name = 'vader';
-    // TODO replace with document.cookie, index into. 
-    return jquery.ajax({
-        url: BASE_URL + DIR_URL + PUB_URL + '/' + name,
-        type: 'GET',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Kryptnostic-Principal', 'krypt|vader');
-            xhr.setRequestHeader('X-Kryptnostic-Credential', 'fd81f8a7af1cb138bdce93350768b2b453ccf238908091501b05fe25616168b0');
-        },
-        success: function(data) {
-            return data;
-        }
-    });
-};
-
-function loadCryptoService(id) {
-    return jquery.ajax({
-        url: BASE_URL + DIR_URL + OBJ_URL + '/' + id,
-        type: 'GET',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Kryptnostic-Principal', 'krypt|vader');
-            xhr.setRequestHeader('X-Kryptnostic-Credential', 'fd81f8a7af1cb138bdce93350768b2b453ccf238908091501b05fe25616168b0');
-        }
-    });
-};
-
-CryptoServiceLoader.prototype = {
-    constructor: CryptoServiceLoader,
-    get: function(id) {
-        var privateKey;
-        var cryptoServiceResponse;
-        jquery.when(
-            getRsaCryptoService(),
-            loadCryptoService(id)
-        ).then(function(rsaCryptoService, cryptoServiceResponse) {
-            console.log(rsaCryptoService);
-            // create RSA crypto service with privateKey
-            console.log(cryptoServiceResponse);
-            // decrypt crypto service, create AES service
+        request.fail(function() {
+            deferred.reject();
         });
-    },
-    set: function(cryptoService) {
-        // RSA encrypt portions of request
-        // send request
-    }
-};
+
+        return deferred.promise();
+    };
+
+    function loadCryptoService(id) {
+        return jquery.ajax({
+            url: BASE_URL + DIR_URL + OBJ_URL + '/' + id,
+            type: 'GET',
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('X-Kryptnostic-Principal', 'krypt|vader');
+                xhr.setRequestHeader('X-Kryptnostic-Credential', 'fd81f8a7af1cb138bdce93350768b2b453ccf238908091501b05fe25616168b0');
+            }
+        });
+    };
+
+    CryptoServiceLoader.prototype = {
+        constructor: CryptoServiceLoader,
+        get: function(id) {
+            var privateKey;
+            var cryptoServiceResponse;
+            jquery.when(
+                getRsaCryptoService.call(this),
+                loadCryptoService(id)
+            ).then(function(rsaCryptoService, cryptoServiceResponse) {
+                console.log(this.rsaCryptoService);
+                // create RSA crypto service with privateKey
+                console.log(cryptoServiceResponse);
+                // decrypt crypto service, create AES service
+            }.bind(this));
+        },
+        set: function(cryptoService) {
+            // RSA encrypt portions of request
+            // send request
+        }
+    };
 
 
-return CryptoServiceLoader;
+    return CryptoServiceLoader;
 });

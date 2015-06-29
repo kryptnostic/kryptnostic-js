d55ad11681ef6d2bd5af760132be9b2208bf8687
Ryan Buckheit <ryan@kryptnostic.com>
2015-06-29 16:13:43 -0700

    update main

--- js/src/main.js
+++ js/src/main.js
@@ -1,20 +1,49 @@
 'use strict';
 
 //
-// Demo script for testing library.
-// All modules requried are loaded from the bulid soteria.js.
+// Demo script for testing library. Loads some kryptnosticObjects from the Kryptnostic backend as a smoke test of library functioanlity.
+// All modules requried are loaded from the built soteria.js, so require.js configuration is not necessary.
 //
 
-require(['require', 'src/utils', 'soteria.crypto-service-loader'], function(require) {
-    var CryptoServiceLoader = require('soteria.crypto-service-loader');
-    var SecurityUtils       = require('src/utils');
 
-    var cryptoServiceLoader = new CryptoServiceLoader("demo");
+var MAX_OBJECTS_TO_LOAD = 5;
 
-    // set credentials
-    sessionStorage.setItem('soteria.principal', 'krypt|demo');
-    sessionStorage.setItem('soteria.credential', 'c1cc09e15a4529fcc50b57efde163dd2a9731d31be629fd9df4fd13bc70134f6');
+require([
+  'require',
+  'soteria.crypto-service-loader',
+  'soteria.storage-client',
+], function(require) {
 
-    // get crypto service
-    cryptoServiceLoader.getObjectCryptoService("8ee6d9af-9916-411d-9720-1f1a5a7f3a4c");
+  var CryptoServiceLoader      = require('soteria.crypto-service-loader');
+  var StorageClient            = require('soteria.storage-client');
+
+  var cryptoServiceLoader      = new CryptoServiceLoader("demo");
+  var storageClient            = new StorageClient();
+
+  // set credentials
+  sessionStorage.setItem('soteria.principal', 'krypt|demo');
+  sessionStorage.setItem('soteria.credential', 'c1cc09e15a4529fcc50b57efde163dd2a9731d31be629fd9df4fd13bc70134f6');
+
+  storageClient.getObjectIds()
+  .done(function(ids) {
+    ids = _.takeRight(ids, MAX_OBJECTS_TO_LOAD);
+    ids.forEach(function(id) {
+      storageClient.getObject(id)
+      .done(function(kryptnosticObject) {
+        cryptoServiceLoader.getObjectCryptoService(id)
+        .done(function (cryptoService) {
+          kryptnosticObject = kryptnosticObject.decrypt(cryptoService)
+
+          $(document).ready(function() {
+            $('body').append("<pre>"
+              + kryptnosticObject.constructor.name + " "
+              + kryptnosticObject.metadata.id
+              + "\n"
+              + JSON.stringify(kryptnosticObject,null,2)
+              + "</pre>");
+          });
+        });
+      });
+    });
+  });
 });

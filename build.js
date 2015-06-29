b7fa292e66a2f6fa93ebdb244407176effa98979
Ryan Buckheit <ryan@kryptnostic.com>
2015-06-29 15:48:37 -0700

    build deps cleanup

--- build.js
+++ build.js
@@ -1,55 +1,23 @@
 (
   {
     baseUrl  : 'js/src',
-    name     : "cs!soteria",
-    out      : "build/soteria.js",
-    optimize : "none",
-    wrap     : true,
+    name     : 'cs!soteria',
+    out      : 'build/soteria.js',
+    optimize : 'none',
+    wrap     : false,
     paths    : {
-      "forge.min"                          : "../lib/forge.min",
-      "jquery"                             : "../lib/jquery",
-      "pako"                               : "../lib/pako",
-      "cookies"                            : "../lib/cookies",
-      "src/utils"                          : "./utils",
-      "src/password-crypto"                : "./password-crypto",
-      "src/rsa-crypto"                     : "./rsa-crypto",
-      "src/aes-crypto"                     : "./aes-crypto",
-      "src/abstract-crypto"                : "./abstract-crypto",
+      'cookies'                            : '../lib/cookies',
+      'forge.min'                          : '../lib/forge.min',
+      'jquery'                             : '../lib/jquery',
+      'pako'                               : '../lib/pako',
+      'lodash'                             : '../lib/lodash',
+      'soteria.crypto-service-loader'      : './crypto-service-loader',
+      'soteria.security-utils'             : './utils',
       'soteria.session-storage-credential' : './session-storage-credential',
-      'soteria.crypto-service-loader'      : './crypto-service-loader'
-    },
-    packages : [
-      {
-        name     : 'cs',
-        location : '../../bower_components/require-cs',
-        main     : 'cs'
-      },
-      {
-        name     : 'coffee-script',
-        location : '../../bower_components/coffeescript',
-        main     : 'extras/coffee-script'
-      }
-    ],
-  },
-
-  {
-    baseUrl  : 'js/src',
-    name     : "cs!soteria",
-    out      : "build/soteria.min.js",
-    optimize : "uglify",
-    wrap     : true,
-    paths    : {
-      "forge.min"                          : "../lib/forge.min",
-      "jquery"                             : "../lib/jquery",
-      "pako"                               : "../lib/pako",
-      "cookies"                            : "../lib/cookies",
-      "src/utils"                          : "./utils",
-      "src/password-crypto"                : "./password-crypto",
-      "src/rsa-crypto"                     : "./rsa-crypto",
-      "src/aes-crypto"                     : "./aes-crypto",
-      "src/abstract-crypto"                : "./abstract-crypto",
-      'soteria.session-storage-credential' : './session-storage-credential',
-      'soteria.crypto-service-loader'      : './crypto-service-loader'
+      'src/abstract-crypto'                : './abstract-crypto',
+      'src/aes-crypto'                     : './aes-crypto',
+      'src/password-crypto'                : './password-crypto',
+      'src/rsa-crypto'                     : './rsa-crypto',
     },
     packages : [
       {

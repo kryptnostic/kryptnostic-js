17341d726612c518520ab4789f0a19891c28fe5e
Ryan Buckheit <ryan@kryptnostic.com>
2015-08-27 17:42:42 -0700

    delete extra copy of client js

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ else
 fi
 
 echo; echo "copying kryptnostic client";
-cp js/lib/KryptnosticClient.js dist/;
+cp bower_components/kryptnostic-client/index.js dist/KryptnosticClient.js;
 
 echo; echo 'compiling demo.coffee...';
 ./node_modules/coffee-script/bin/coffee -c demo/demo.coffee;
